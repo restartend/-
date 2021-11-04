07335ebd3012fe54278c1a050f132f295300ff71
橘加薄荷 <78253718+restartend@users.noreply.github.com>
2021-11-04 22:52:52 +0800

    Add files via upload

---
+++ project.java
@@ -0,0 +1,34 @@
+package sele;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import javax.swing.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.lang.String;
+public class project {
+    public static void main(String[] args) throws Exception {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://haokan.baidu.com/?fr=pc_pz");
+        //WebElement kk = driver.findElement(By.className("search-wrap float-left"));
+        WebElement CK = driver.findElement(By.xpath("//input[@id='search_input']"));
+        WebElement bottom = driver.findElement(By.xpath("//div[@class='search-btn float-right']"));
+        //CK.click();
+        System.out.println(CK.getClass());
+        Thread.sleep(1000);
+        CK.sendKeys("起飞");
+        bottom.click();
+        WebDriver adTWO=new ChromeDriver();
+        String ad=adTWO.getCurrentUrl();
+        adTWO.get(ad);
+        //int mark[10];
+        for(int time=0;time<=7;time++)
+        {
+            WebElement address = adTWO.findElement(By.xpath("//class[@class='list-container videolist clearfix']"));
+            System.out.println(address.getClass());
+        }
+    }
+}
